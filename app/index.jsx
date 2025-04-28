032ae2cd196b34f374c69fbbfc5bd1509c2799a0
Trevor Foster <trevafoser@gmail.com>
2025-04-28 16:20:55 -0500

    Add Navigation

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, Image } from "react-native";
+import { Link } from "expo-router";
+
 import Logo from "../assets/img/logo_light.png";
-import React from "react";
 
 const Home = () => {
   return (
@@ -10,6 +11,13 @@ const Home = () => {
       <Text style={styles.title}>The Number 1</Text>
 
       <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+
+      <Link href="/about" style={styles.link}>
+        About Page
+      </Link>
+      <Link href="/contact" style={styles.link}>
+        Contact Page
+      </Link>
     </View>
   );
 };
@@ -28,5 +36,9 @@ const styles = StyleSheet.create({
   },
   img: {
     marginVertical: 20,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
   },
 });

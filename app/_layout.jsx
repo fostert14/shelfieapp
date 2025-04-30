21532bb97baac59e941dec52d630adf91796c9f8
Trevor Foster <trevafoser@gmail.com>
2025-04-30 12:11:44 -0500

    Add _layout

---
+++ app/_layout.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Stack } from 'expo-router'
+
+const RouteLayout = () => {
+  return ( 
+    <Stack screenOptions={{
+        headerStyle: { backgroundColor: '#ddd' },
+        headerTintColor: '#333',
+    }}>
+        <Stack.Screen name="index" options={{title: 'Home' }} />
+        <Stack.Screen name="about" options={{title: 'About' }} />
+        <Stack.Screen name="contact" options={{title: 'Contact', headerShown: false }} />
+    </Stack>
+  )
+}
+
+export default RouteLayout
+
+const styles = StyleSheet.create({})

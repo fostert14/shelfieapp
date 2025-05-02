f6f7c761b24e8b29d32b37c38ece437e455de3bb
Trevor Foster <trevafoser@gmail.com>
2025-05-02 12:53:36 -0500

    Add: Individual Theme Components

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,25 +1,26 @@
 import { StyleSheet, Text, useColorScheme, View } from "react-native";
 import { Stack } from "expo-router";
 import { Colors } from "../constants/Colors";
+import { StatusBar } from "expo-status-bar";
 
 const RouteLayout = () => {
   const colorScheme = useColorScheme();
-  const theme = Colors[colorScheme] ?? Colors.light;
+  const theme = Colors[colorScheme] ?? Colors.dark;
 
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: theme.navBackground },
-        headerTintColor: theme.title,
-      }}
-    >
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="about" options={{ title: "About" }} />
-      <Stack.Screen
-        name="contact"
-        options={{ title: "Contact", headerShown: false }}
-      />
-    </Stack>
+    <>
+      <StatusBar value="auto" />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="about" options={{ title: "About" }} />
+        <Stack.Screen name="contact" options={{ title: "Contact" }} />
+      </Stack>
+    </>
   );
 };
 

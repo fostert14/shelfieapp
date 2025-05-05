fe16557887fdd4bfb43ad243a3c62e92198a3f38
Trevor Foster <trevafoser@gmail.com>
2025-05-05 16:17:20 -0500

    Add: Tabs

--- app/index.jsx
+++ app/index.jsx
@@ -30,6 +30,9 @@ const Home = () => {
       <Link href="/register" style={styles.link}>
         <ThemedText>Register Page</ThemedText>
       </Link>
+      <Link href="/profile" style={styles.link}>
+        <ThemedText>Profile Page</ThemedText>
+      </Link>
     </ThemedView>
   );
 };

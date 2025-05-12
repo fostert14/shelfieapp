78b2365ff7089adbbcd5ab43e8f291a4f2f6b6de
Trevor Foster <trevafoser@gmail.com>
2025-05-12 13:49:40 -0500

    Add: Display errors

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -5,6 +5,7 @@ import {
   Keyboard,
 } from "react-native";
 import { Link } from "expo-router";
+import { Colors } from "../../constants/Colors";
 import { useState } from "react";
 
 // themed components
@@ -19,13 +20,18 @@ import { useUser } from "../../hooks/useUser";
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const { register } = useUser();
 
   const handleSubmit = async () => {
+    setError(null);
+
     try {
       await register(email, password);
-    } catch (error) {}
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   return (
@@ -56,6 +62,9 @@ const Register = () => {
           <Text style={{ color: "#f2f2f2" }}>Register</Text>
         </ThemedButton>
 
+        <Spacer />
+        {error && <Text style={styles.error}>{error}</Text>}
+
         <Spacer height={100} />
         <Link href="/login">
           <ThemedText style={{ textAlign: "center" }}>Login Instead</ThemedText>
@@ -77,5 +86,14 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30,
+  },
+  error: {
+    color: Colors.warning,
+    padding: 10,
+    backgroundColor: "#f5c1c8",
+    borderColor: Colors.warning,
+    borderWidth: 1,
+    borderRadius: 6,
+    marginHorizontal: 10,
   },
 });

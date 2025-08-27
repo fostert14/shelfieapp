2da2ce1fe6a785d05dd0ea292c8815bd077ddd55
Trevor Foster <trevafoser@gmail.com>
2025-08-26 20:22:32 -0500

    Add: Deleting Book

--- app/(dashboard)/books/[id].jsx
+++ app/(dashboard)/books/[id].jsx
@@ -1,7 +1,8 @@
-import { StyleSheet } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { StyleSheet, Text } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
 import { useBooks } from "../../../hooks/useBooks";
+import { Colors } from "../../../constants/Colors";
 
 // themed components
 import ThemedText from "../../../components/ThemedText";
@@ -16,7 +17,14 @@ const BookDetails = () => {
 
   const { id } = useLocalSearchParams();
 
-  const { fetchBookById } = useBooks();
+  const { fetchBookById, deleteBook } = useBooks();
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    await deleteBook(id);
+    setBook(null);
+    router.replace("/books");
+  };
 
   useEffect(() => {
     async function loadBook() {
@@ -47,6 +55,10 @@ const BookDetails = () => {
 
         <ThemedText>{book.description}</ThemedText>
       </ThemedCard>
+
+      <ThemedButton style={styles.delete} onPress={handleDelete}>
+        <Text style={{ color: "#fff", textAlign: "center" }}>Delete Book</Text>
+      </ThemedButton>
     </ThemedView>
   );
 };
@@ -64,5 +76,12 @@ const styles = StyleSheet.create({
   },
   card: {
     margin: 20,
+  },
+
+  delete: {
+    marginTop: 40,
+    backgroundColor: Colors.warning,
+    width: 200,
+    alignSelf: "center",
   },
 });
